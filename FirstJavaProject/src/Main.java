d780f1af184fe08f6a8033de2c98f5cc4049215a
SylviaIfy <ifeomaofiani@gmail.com>
2023-03-14 22:48:54 +0100

    Added my First Java Project files

---
+++ FirstJavaProject/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        }
+    static {
+        System.out.println("Hello Testify!");
+    }
+    static {
+        System.out.println("This is my first Java script on IntelliJ.");
+    }
+    static {
+        System.out.println("These are my first Java code lines, and I'm doing this to figure out how it all works, but I believe this will be an interesting ride.");
+    }
+    static {
+        System.out.println("I'm Can-Do Sylvia, and I'm absolutely ready to take the world of Software Testing, Java and Tech by storm.");
+    }
+    static {
+        System.out.println("See you again soon, Cheers!");
+    }
+}
